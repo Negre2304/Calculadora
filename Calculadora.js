1bd33645cf21fb92efe5f3295d3223d24e069aa3
Negre2304 <negre.victor@hotmail.com>
2022-03-17 19:49:16 -0300

    Completed Calculator

--- Calculadora.js
+++ Calculadora.js
@@ -1,7 +1,7 @@
-function valor() {
+function valor(numero) {
 
     var res = document.getElementById('resultado');
-    //res.innerHTML += 
+    res.innerHTML += numero; 
 }
 
 function clean() {
@@ -10,3 +10,16 @@ function clean() {
     res.innerHTML = ''
 }
 
+function calculo() {
+
+    var resultado = document.getElementById('resultado').innerHTML;
+
+    if(resultado) {
+
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+
+        alert(`[ERRO] Não há números para realizar o calculo!`)
+    }
+}
