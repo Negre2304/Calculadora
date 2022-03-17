02ae28b8a8f5a44a2c8944acf4c526400327bee6
Negre2304 <negre.victor@hotmail.com>
2022-03-17 16:43:40 -0300

    updade calculator

--- Calculadora.js
+++ Calculadora.js
@@ -1,2 +1,11 @@
-//segura ae
-//tmj
+function valor() {
+
+    var res = document.getElementById('resultado');
+    //res.innerHTML += 
+}
+
+function clean() {
+
+    var res = document.getElementById('resultado');
+    res.innerHTML = ''
+}
